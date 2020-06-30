929dfa215f6a837fe1e558142bac5593723a1863
leowcy <leowcy@gwmail.gwu.edu>
2020-06-29 23:12:41 -0400

    AC

--- doubleLink/141-linkedListCycle.js
+++ doubleLink/141-linkedListCycle.js
@@ -23,5 +23,15 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    
+    if (head == null || head.next == null) return false;
+    let slow = head;
+    let fast = head.next;
+
+    while (slow != fast) {
+        if (fast == null || fast.next == null) return false;
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return true;
 };
