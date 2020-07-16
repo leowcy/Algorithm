e431354c0efba407ba3db8b06af20d99a9947e45
leowcy <leowcy@gwmail.gwu.edu>
2020-07-15 23:53:27 -0400

    add insertsort algo

--- sort/sort.js
+++ sort/sort.js
@@ -1,6 +1,6 @@
 // Put All the existing sort algorithm here
 
-let array = [2,4,55,5,6,2,123,5,23541,-1,0,555,6,88,-3,-5];
+let array = [2,4,55,5,6,2,123,5,23541,-1,0,555,6,88,-3,-5, 100024];
 
 //1. Bubble Sort
 // Swap the neighbor element only - slowest. Put the largest element to the end of the array
@@ -42,3 +42,37 @@ function selectSort(arr) {
 }
 
 //selectSort(array);
+
+//3. Insert sort
+/**
+ * 将第一待排序序列第一个元素看做一个有序序列，把第二个元素到最后一个元素当成是未排序序列。
+从头到尾依次扫描未排序序列，将扫描到的每个元素插入有序序列的适当位置。（如果待插入的元素与有序序列中的某个元素相等，则将待插入元素插入到相等元素的后面。）
+ */
+
+function insertSort(arr) {
+    const len = arr.length;
+    let arrToSort = [arr[0]];
+    for(let i = 1; i < len; i++) {
+        arrToSort = insert(arrToSort, arr[i]);
+    }
+    console.log("arrToSort: ", arrToSort);
+    return arrToSort;
+}
+
+const insert = (insertArr, insertValue) => {
+    for(let j = 0; j < insertArr.length; j++) {
+        if (j == 0 && insertArr[j] >= insertValue) {
+            insertArr.unshift(insertValue);
+            break;
+        } else if (j == insertArr.length - 1 && insertArr[j] <= insertValue) {
+            insertArr.push(insertValue);
+            break;
+        } else if (j != 0 && j != insertArr.length-1 && insertArr[j] <= insertValue && insertArr[j+1] >= insertValue) {
+            insertArr.splice(j, 0, insertValue);
+            break;
+        }
+    }
+    return insertArr;
+}
+
+insertSort(array);
