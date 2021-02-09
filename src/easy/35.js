255b5725083b14c6e651c38dcff51227970c9fb1
leowcy <leowcy@gwmail.gwu.edu>
2021-02-08 19:07:30 -0500

    AC 35

---
+++ src/easy/35.js
@@ -0,0 +1,27 @@
+/**
+ * 35. Search Insert Position
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ * Example 1:
+ * Input: nums = [1,3,5,6], target = 5
+ * Output: 2
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    const lengthOfNums = nums.length;
+    if (!nums || lengthOfNums === 0) {
+        return 0;
+    }
+    for (let i = 0; i < lengthOfNums; i++) {
+        if (nums[i] >= target) {
+            return i;
+        }
+        if (i === lengthOfNums - 1) {
+            return lengthOfNums;
+        }
+    }
+};
