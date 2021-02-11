c522e3d675598552a9919ddb3370dbdd80dd83e4
leowcy <leowcy@gwmail.gwu.edu>
2021-02-11 00:40:02 -0500

    AC 69 - oneline

---
+++ src/easy/69.js
@@ -0,0 +1,15 @@
+/**
+ * 69. Sqrt(x)
+ * Given a non-negative integer x, compute and return the square root of x.
+ * Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+ */
+
+/**
+* @param {number} x
+* @return {number}
+*/
+var mySqrt = function (x) {
+    return Math.floor(Math.sqrt(x))
+};
+
+console.log(mySqrt(0))
