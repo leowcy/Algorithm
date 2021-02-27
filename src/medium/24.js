bc2d57a1eeaa2c06f1e39d70deea17fadadd60a0
leowcy <leowcy@gwmail.gwu.edu>
2021-02-27 13:44:53 -0500

    24 - Add recusion approach

--- src/medium/24.js
+++ src/medium/24.js
@@ -18,7 +18,7 @@ var swapPairs = function (head) {
     if (!head) return null;
     let newHead = new ListNode(0, head);
     let tempNode = newHead;
-    while(head && head.next) {
+    while (head && head.next) {
         let temp = head.next.next;
         head.next.next = head;
         tempNode.next = head.next;
@@ -27,4 +27,28 @@ var swapPairs = function (head) {
         head = head.next;
     }
     return newHead.next;
+};
+
+/**
+ * sudo code:
+ * a. 1 -> 2 -> 3
+ * b. 0 -> 1 -> 2 -> 3
+ * c. 0 -> 1 <->2 ... 3
+ * d. 0 -> 2 <-> 1 ... 3
+ * e. 0 -> 2 -> 1 -> 3
+ * f. move 0 to 1 position and move head(1) to head.next
+ */
+
+// 2. Recursion approach
+
+var swapPairs = function (head) {
+    if (head == null || head.next == null) return head;
+
+    const next = head.next;
+    const res = swapPairs(next.next);
+
+    head.next = res;
+    next.next = head;
+
+    return next;
 };
