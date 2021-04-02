50982d67782a14fc74d036f16d5c06b43ab7d32f
leowcy <leowcy@gwmail.gwu.edu>
2021-04-02 14:03:23 -0400

    AC-10-机器人运动DFS/BFS

---
+++ src/JianzhiOffer/10_机器人的运动范围/main.js
@@ -0,0 +1,55 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+ var movingCount = function (m, n, k) {
+    if (k == 0) return 1;
+
+    let blockSet = new Set();
+
+    var dfs = (row, column) => {
+        let sum = getSum(row, column);
+        let rowColumnString = row.toString() + ',' + column.toString();
+        if (row < 0 || row >= m || column < 0 || column >= n || sum > k || blockSet.has(rowColumnString)) return;
+
+        // set checked block as used
+        blockSet.add(rowColumnString);
+        dfs(row + 1, column);
+        dfs(row - 1, column);
+        dfs(row, column + 1);
+        dfs(row, column - 1);
+    }
+
+    dfs(0, 0);
+    return blockSet.size;
+};
+
+// get sum using the reduce function call
+var getSum = (a, b) => {
+    let digitOfA = a.toString().split(''),
+        digitOfB = b.toString().split('');
+    const digitCombination = digitOfA.concat(digitOfB);
+    return digitCombination.reduce((a, b) => Number(a) + Number(b), 0)
+}
+
+// get sum using the math
+var getSum1 = (a, b) => {
+    let sumOfA = 0, sumOfB = 0;
+
+    while (a) {
+        sumOfA += a % 10;
+        a = Math.floor(a / 10);
+    }
+
+    while (b) {
+        sumOfB += b % 10;
+        b = Math.floor(b / 10);
+    }
+
+    return sumOfA + sumOfB;
+}
+
+
+//movingCount(1,2,1)
---
+++ src/JianzhiOffer/10_机器人的运动范围/readme.md
@@ -0,0 +1,26 @@
+### 机器人的运动范围
+
+地上有一个 m 行 n 列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。一个机器人从坐标 [0, 0] 的格子开始移动，它每次可以向左、右、上、下移动一格（不能移动到方格外），也不能进入行坐标和列坐标的数位之和大于 k 的格子。例如，当 k 为 18 时，机器人能够进入方格 [35, 37] ，因为 3+5+3+7=18。但它不能进入方格 [35, 38]，因为 3+5+3+8=19。请问该机器人能够到达多少个格子？
+
+示例 1：
+输入：m = 2, n = 3, k = 1
+输出：3
+
+示例 2：
+输入：m = 3, n = 1, k = 0
+输出：1
+
+提示：
+1 <= n,m <= 100
+0 <= k <= 20
+
+## solution 1:
+
+- Similar to question No.9 -> Move up/down/left/right -> so easily think of using DFS/BFS. -> 此题为广度优先
+- 几个关键点：
+  1. 位数和的计算 （写了两种方法在 main.js 中）
+  2. 四周方向遍历（dfs() row +- 1 / column +- 1)
+  3. 限制条件（不能越界 + 位数和大于K + 该格子未被检索过）
+  4. 统计格子数（可以设定一个blockNumber， 更巧妙的是在3中，检索格子使用情况的时候会设置一个set，最终set的长度即为格子数）
+- Time Complexity: O(n) = mn (row and column) -> The same block could be visit multiple times but it will only be a constant value. So c*mn = mn
+- Space Complexity: O(n) = mn -> there is a set to check if the block has been verified or not -> the set will be m*n space
