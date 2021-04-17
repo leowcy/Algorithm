393b8a8383d95482eaae3f89b3c8e52c9ef2ebcc
leowcy <leowcy@gwmail.gwu.edu>
2021-04-17 13:55:29 -0400

    AC-29-从上到下打印二叉树的两种变形题目

--- src/JianzhiOffer/29_从上到下打印二叉树/main.js
+++ src/JianzhiOffer/29_从上到下打印二叉树/main.js
@@ -23,3 +23,90 @@ var levelOrder = function (root) {
 
     return data;
 };
+
+// 第二题：按层打印结果
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+    if (!root) return [];
+
+    const queue = [[root, 0]], data = [];
+
+    while (queue.length) {
+        let [temp, level] = queue.shift();
+        if (!data[level]) data[level] = [];
+        data[level].push(temp.val);
+
+        temp.left && queue.push([temp.left, level + 1]);
+        temp.right && queue.push([temp.right, level + 1]);
+    }
+
+    return data;
+};
+
+// 题目变形3：Zigzag print out binary tree
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+    if (!root) return [];
+
+    const queue = [[root, 0]], data = [];
+
+    while (queue.length) {
+        let [temp, level] = queue.shift();
+        if (!data[level]) data[level] = [];
+        data[level].push(temp.val);
+
+        temp.left && queue.push([temp.left, level + 1]);
+        temp.right && queue.push([temp.right, level + 1]);
+    }
+
+    // 输出时进行奇数层逆序
+    for (let i = 1; i < data.length; i++) {
+        if (i % 2 == 1) {
+            data[i].reverse();
+        }
+    }
+    return data;
+};
+
+//解法2：
+var levelOrder = function (root) {
+    if (!root) return [];
+
+    const queue = [[root, 0]], data = [];
+
+    while (queue.length) {
+        let [temp, level] = queue.shift();
+        if (!data[level]) data[level] = [];
+        if (level & 1) {
+            // 奇数层头插
+            data[level].unshift(temp.val);
+        } else
+            // 偶数层尾插
+            data[level].push(temp.val);
+
+        temp.left && queue.push([temp.left, level + 1]);
+        temp.right && queue.push([temp.right, level + 1]);
+    }
+
+    return data;
+};
