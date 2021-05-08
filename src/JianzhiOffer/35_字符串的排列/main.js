30c483535b3c364870e5de372783488739d536b5
leowcy <leowcy@gwmail.gwu.edu>
2021-05-07 22:09:18 -0400

    AC-35-permutation

---
+++ src/JianzhiOffer/35_字符串的排列/main.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var permutation = function (s) {
+    const res = new Set();
+    const visit = {};
+    function dfs(path) {
+        if (path.length == s.length) return res.add(path);
+        for (let i = 0; i < s.length; i++) {
+            if (visit[i]) continue;
+            visit[i] = true;
+            // recursion here
+            dfs(path + s[i]);
+            visit[i] = false;
+        }
+    }
+    dfs('');
+    // change set to array
+    return [...res];
+};
