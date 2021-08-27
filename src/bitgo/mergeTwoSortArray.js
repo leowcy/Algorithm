8ed82fd0ae50b106621990f9cb1d7ce5980cb24c
leowcy <leowcy@gwmail.gwu.edu>
2021-08-27 15:11:15 -0400

    AC-merge two sort array

---
+++ src/bitgo/mergeTwoSortArray.js
@@ -0,0 +1,16 @@
+var merge = function (nums1, m, nums2, n) {
+    
+    var sum = m + n;
+    m--;
+    n--;
+    
+    while (sum--) {
+        if (n < 0 || nums1[m] >= nums2[n]) {
+            nums1[sum] = nums1[m--];
+        } else {
+            nums1[sum] = nums2[n--];
+        }
+    }
+    
+    return nums1;
+};
