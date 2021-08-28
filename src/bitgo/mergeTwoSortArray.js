bd04b7612ada21fcbd9f2e01c3f4e9f037cf3ee3
leowcy <leowcy@gwmail.gwu.edu>
2021-08-27 22:21:26 -0400

    merge two sort array

--- src/bitgo/mergeTwoSortArray.js
+++ src/bitgo/mergeTwoSortArray.js
@@ -1,9 +1,9 @@
 var merge = function (nums1, m, nums2, n) {
-    
+
     var sum = m + n;
     m--;
     n--;
-    
+
     while (sum--) {
         if (n < 0 || nums1[m] >= nums2[n]) {
             nums1[sum] = nums1[m--];
@@ -11,6 +11,6 @@ var merge = function (nums1, m, nums2, n) {
             nums1[sum] = nums2[n--];
         }
     }
-    
+
     return nums1;
 };
