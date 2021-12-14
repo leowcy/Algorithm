6334fb2fe070200d92505b6e03f36547ae8f4f58
leowcy <leowcy@gwmail.gwu.edu>
2021-12-14 17:34:45 -0500

    rewrite compart N tree

--- src/doorD/compareTree/compartNTreeNode.js
+++ src/doorD/compareTree/compartNTreeNode.js
@@ -9,19 +9,19 @@ class TreeNode {
 const root1 = new TreeNode(1, 'A');
 root1.children.push(new TreeNode(2, 'B'));
 root1.children.push(new TreeNode(3, 'C'));
-root1.children[0].children.push(new TreeNode(4, 'D'));                               
+root1.children[0].children.push(new TreeNode(4, 'D'));
 root1.children[0].children.push(new TreeNode(5, 'E'));
 root1.children[0].children.push(new TreeNode(6, 'F'));
 root1.children[1].children.push(new TreeNode(7, 'G'));
 root1.children[1].children.push(new TreeNode(8, 'H'));
 
 const root2 = new TreeNode(1, 'A');
-root2.children.push(new TreeNode(2, 'B'))
-root2.children.push(new TreeNode(33, 'C'))
-root2.children[0].children.push(new TreeNode(5, 'E'))
-root2.children[0].children.push(new TreeNode(4, 'D'))
-root2.children[0].children.push(new TreeNode(22, 'F'))
-root2.children[1].children.push(new TreeNode(7, 'G'))
+root2.children.push(new TreeNode(2, 'B'));
+root2.children.push(new TreeNode(33, 'C'));
+root2.children[0].children.push(new TreeNode(5, 'E'));
+root2.children[0].children.push(new TreeNode(4, 'D'));
+root2.children[0].children.push(new TreeNode(22, 'F'));
+root2.children[1].children.push(new TreeNode(7, 'G'));
 root2.children[1].children.push(new TreeNode(8, 'I'));
 
 console.log(root1)
@@ -43,58 +43,47 @@ var countNode = function (node) {
 }
 
 var findDiff = function (r1, r2) {
-    // border conditions
-    if (!r1 && !r2) {
-        return 0;
-    }
+    if (!r1 && !r2) return 0;
 
-    // if only one node exists or the key between both nodes are different
     if (!r1 || !r2 || r1.key != r2.key) {
         return countNode(r1) + countNode(r2);
     }
 
-    // if both node exist and share the same key, calculate the case when the value is different. If the value are also the same, we don't need to consider it.
-    let diffCount = 0;
+    let res = 0;
     if (r1.value != r2.value) {
-        diffCount += 2;
+        res += 2;
+    }
+    // check their children
+    let c1 = r1.children,
+        c2 = r2.children;
+
+    let c1Map = new Map(),
+        c2Map = new Map();
+
+    for (const child of c1) {
+        c1Map.set(child.key, child);
     }
 
-    // compare the children node of them
-    const c1 = r1.children;
-    const c2 = r2.children;
-    let c1Map = new Map();
-    let c2Map = new Map();
-
-    // init two hash map c1Map & c2Map
-    for (const child1 of c1) {
-        c1Map.set(child1.key, child1);
-    }
-    for (const child2 of c2) {
-        c2Map.set(child2.key, child2);
+    for (const child of c2) {
+        c2Map.set(child.key, child);
     }
 
-    let testVal;
-    // compare two map
-    for (var [key, val] of c1Map) {
-        testVal = c2Map.get(key);
-        // if no key found, countNode for the current branch which is val
-        if (!key) {
-            diffCount += countNode(val);
+    for (const [key, val] of c1Map) {
+        let c2Val = c2Map.get(key);
+        if (!c2Val) {
+            res += countNode(val);
         } else {
-            // if share the same key, no matter val is same or not, keep recursively search for the children
-            diffCount += findDiff(val, testVal);
-            // remove the key from c2Map after calculation
+            res += findDiff(val, c2Val);
             c2Map.delete(key);
         }
     }
 
-    // if c2Map still has some node remaining, count and add them to the diffCount
-    for (var [key, val] of c2Map) {
-        diffCount += countNode(val);
+    // if c2Map still has some remaining values
+    for (const [key, val] of c2Map) {
+        res += countNode(val);
     }
 
-    // return the result
-    return diffCount;
+    return res;
 }
 
 const numsOfDifferentNodes = findDiff(root1, root2);
