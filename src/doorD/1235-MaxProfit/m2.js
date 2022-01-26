33b1648b0037658d28d0643cf06d2047efa6ee4a
leowcy <leowcy@gwmail.gwu.edu>
2022-01-25 21:11:09 -0500

    while loop left <= right

--- src/doorD/1235-MaxProfit/m2.js
+++ src/doorD/1235-MaxProfit/m2.js
@@ -42,14 +42,14 @@ var fcj = function (job, index) {
     let l = 0;
     let r = index - 1;
     let res = -1;
-    while (l < r) {
+    while (l <= r) {
         let mid = Math.floor((l + r) / 2);
         if (job[mid].endTime <= job[index].startTime) {
             // qualified
             res = mid;
             l = mid + 1;
         } else {
-            r = mid;
+            r = mid - 1;
         }
     }
     return res;
