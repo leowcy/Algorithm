ea8977fc6ea09150a9eb8eb8128338765189f9ff
leowcy <leowcy@gwmail.gwu.edu>
2022-03-24 17:20:52 -0400

    AC-77-dfs

---
+++ src/LeetCode/medium/77-Combination.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function (n, k) {
+
+    let res = [];
+
+    // main code
+    var helper = function (str, index) {
+        if (str.length == k) {
+            res.push([...str]);
+            return;
+        }
+
+        for (let i = index; i <= n; i++) {
+            str.push(i);
+            helper(str, i + 1);
+            str.pop();
+        }
+
+        return;
+    }
+
+    helper([], 1);
+
+    return res;
+};
