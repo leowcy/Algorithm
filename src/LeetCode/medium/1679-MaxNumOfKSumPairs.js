4feb62320df0d9d0c9be08eb8de186e32c8e615b
leowcy <leowcy@gwmail.gwu.edu>
2022-05-04 17:23:28 -0400

    AC-581-1679-DC

---
+++ src/LeetCode/medium/1679-MaxNumOfKSumPairs.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxOperations = function (nums, k) {
+    if (!nums || nums.length == 0) return 0;
+
+    nums.sort((a, b) => a - b);
+
+    let left = 0,
+        right = nums.length - 1;
+
+
+    let res = 0;
+    while (left < right) {
+        if (nums[left] + nums[right] == k) {
+            left++;
+            right--;
+            res++;
+        } else if (nums[left] + nums[right] < k) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return res;
+};
--- src/LeetCode/medium/581-ShorestUnsortedContinuousSubArr.js
+++ src/LeetCode/medium/581-ShorestUnsortedContinuousSubArr.js
@@ -52,3 +52,31 @@ var findUnsortedSubarray = function (nums) {
  * 4. Similarly - Start from the end of the array - Once found the nums[j] value is smaller than max - Means we found the right border of the output
  * 5. Return the value base on conditions
  */
+
+ var findUnsortedSubarray1 = function(nums) {
+    // Left === the start of the subarray while right === the end of the subarray
+    let left, right, largest = - Infinity;
+    
+    for (let i = 0; i < nums.length; i++) {
+        largest = Math.max(largest, nums[i]);
+
+        // If the current value is less than the largest number encountered, 
+        // then the current idx must be included in the to-be-sorted subarray, so expand the subarray
+        if (nums[i] < largest) {
+            if (left === undefined) {
+                left = i - 1;
+            }
+            // If the current value is less than the value at the start of the subarray, expand the subarray start leftwards
+            while (left >= 0 && nums[i] < nums[left]) {
+                left--;
+            }
+            // Expand the subarray end to the current index
+            right = i;
+        }
+    }
+    
+    if (left === undefined && right === undefined) return 0;
+    else return right - left;
+};
+
+findUnsortedSubarray1([5,4,3,2,1])
