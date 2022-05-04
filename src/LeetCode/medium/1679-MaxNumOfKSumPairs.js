8f0af64a75fc539e43d0901598ca8c49d373b85a
leowcy <leowcy@gwmail.gwu.edu>
2022-05-04 17:35:44 -0400

    AC-1679-hashmap solution

--- src/LeetCode/medium/1679-MaxNumOfKSumPairs.js
+++ src/LeetCode/medium/1679-MaxNumOfKSumPairs.js
@@ -3,6 +3,8 @@
  * @param {number} k
  * @return {number}
  */
+
+// Solution 1: O(n) = nlogN time - O(1) space
 var maxOperations = function (nums, k) {
     if (!nums || nums.length == 0) return 0;
 
@@ -22,6 +24,34 @@ var maxOperations = function (nums, k) {
             left++;
         } else {
             right--;
+        }
+    }
+
+    return res;
+};
+
+// solution 2: Hashmap - O(n) time & O(n) space
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxOperations = function (nums, k) {
+    if (!nums || nums.length == 0) return 0;
+
+    let numsMap = new Map(),
+        res = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (numsMap.get(k - nums[i])) {
+            if (numsMap.get(k - nums[i]) == 1) {
+                numsMap.delete(k - nums[i]);
+            } else {
+                numsMap.set(k - nums[i], numsMap.get(k - nums[i]) - 1);
+            }
+            res++;
+        } else {
+            numsMap.set(nums[i], numsMap.get(nums[i]) + 1 || 1);
         }
     }
 
