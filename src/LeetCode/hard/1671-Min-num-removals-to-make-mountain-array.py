b9a79cfb5f4c874d7deec30885d1c49f701bad1b
leowcy <leowcy@gwmail.gwu.edu>
2024-10-30 17:39:30 -0400

    add fix to 1671

--- src/LeetCode/hard/1671-Min-num-removals-to-make-mountain-array.py
+++ src/LeetCode/hard/1671-Min-num-removals-to-make-mountain-array.py
@@ -28,21 +28,21 @@
 
 
 class Solution:
-    def minimumMountainRemovals(self, nums: list[int]) -> int:
+    def minimumMountainRemovals(self, nums: List[int]) -> int:
         
         n = len(nums)
         lis = [1] * n
         lds = [1] * n
 
         for i in range(n):
-            for j in range(lis):
+            for j in range(i):
                 if nums[i] > nums[j]:
                     lis[i] = max(lis[i], lis[j] + 1)
         
 
         for i in range(n-1, -1, -1):
             for j in range(n-1, i, -1):
-                if nums[i] < nums[j]:
+                if nums[i] > nums[j]:
                     lds[i] = max(lds[i], lds[j] + 1)
         
         maxMountainLength = 0
