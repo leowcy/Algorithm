67b0dd937605da351c2f0521e76a90a3b24af0d9
leowcy <leowcy@gwmail.gwu.edu>
2024-11-06 14:51:22 -0500

    Keep doing 909 3rd time for BFS

--- src/top150/909-snakes-and-ladders.py
+++ src/top150/909-snakes-and-ladders.py
@@ -1,41 +1,45 @@
-class Solution:
-    def snakesAndLadders(self, board: List[List[int]]) -> int:
-        n = len(board)
-        nums = n * n
-        ladder = dict()
+def snakesAndLadders(board: list[list[int]]) -> int:
+    n = len(board)
+    nums = n * n
+    ladder = dict()
 
-        for j in range(n):
-            for k in range(n):
-                if board[j][k] != -1:
-                    # check odd or event layer. remember, start from bottom left.
-                    if (n - 1 - j) % 2 == 1:
-                        to_add = n - k
-                    else:
-                        to_add = k + 1
-                    
-                    to_add += (n - 1 - j) * n
-                    ladder[to_add] = board[j][k]
-        
-        steps = 0
-        visited = [0 for _ in range(nums+1)]
-        visited[1] = 1
-        curr = [1]
+    for j in range(n):
+        for k in range(n):
+            if board[j][k] != -1:
+                if (n - 1 - j) % 2 == 1:
+                    num = n - k
+                else:
+                    num = k + 1
+                num += (n - 1 - j) * n
+                ladder[num] = board[j][k]
+    
+    # print(ladder)
 
-        while curr:
-            steps += 1
-            new = set()
-            for j in curr:
-                if j + 6 >= nums:
-                    return steps
-                for k in range(j+1, j+7):
-                    if k in ladder and (not visited[ladder[k]]):
-                        if ladder[k] == nums:
-                            return steps
-                        visited[ladder[k]] = 1
-                        new.add(ladder[k])
-                    elif k not in ladder and (not visited[k]):
-                        visited[k] = 1
-                        new.add(k)
-            curr = new
+    visited = [0 for _ in range(nums+1)]
+    visited[1] = 1
+    steps = 0
+    curr = [1]
+
+    while curr:
+        steps += 1
+        new = set()
+        for j in curr:
+            if nums - j <= 6:
+                return steps
+            for k in range(j+1, j+7): # to 7 since 7 not included
+                if k in ladder and (not visited[ladder[k]]):
+                    if ladder[k] == nums:
+                        return steps
+                    visited[ladder[k]] = 1
+                    new.add(ladder[k])
+                elif k not in ladder and (not visited[k]):
+                    visited[k] = 1
+                    new.add(k)
+        curr = new
+    
+    return -1
             
-        return -1
+    
+
+board = [[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,35,-1,-1,13,-1],[-1,-1,-1,-1,-1,-1],[-1,15,-1,-1,-1,-1]]
+print(snakesAndLadders(board=board))
