cf7beb9588ad6f6e1798cf44a848f420bec478ee
leowcy <leowcy@gwmail.gwu.edu>
2024-11-26 22:12:24 -0500

    AC

--- src/other/best-day-cross-mountain.py
+++ src/other/best-day-cross-mountain.py
@@ -12,3 +12,55 @@
 # 寻找出最好一天穿越雪山。注意，穿越雪山只能最多向上或者向下移动高度为1。如果高度落差为2，则这一天无法爬过雪山。
 # 寻找哪一天能顺利通过雪山，并且最少得攀爬数。
 # ###
+
+def best_day_cross_mountain(m, s):
+
+    m_init = m.copy()
+
+    def _if_can_pass_mountain(high: list[int]) -> int:
+        prev = high[0]
+        effort = 0
+        for i in range(1, len(high)):
+            if abs(high[i] - prev) > 1:
+                return -1
+            effort += abs(high[i] - prev)
+            prev = high[i]
+        
+        return effort
+    
+    no_snow = 0
+    res = []
+    for index, day in enumerate(s):
+        if sum(day) == 0:
+            no_snow += 1
+            if no_snow == 2:
+                no_snow = 0
+                # remove snow on mountain if there is snow
+                for i, _ in enumerate(m):
+                    m[i] = m[i] - 1 if m[i] > m_init[i] else m_init[i]
+                # print(m)
+                # print(m_init)
+        else:
+            no_snow = 0
+            for peak, snow in enumerate(day):
+                m[peak] += snow
+        
+        # print(m)
+
+        if _if_can_pass_mountain(m) != -1:
+            res.append([index, _if_can_pass_mountain(m)])
+        
+    # print(res)
+    if res:
+        ans = res[0]
+        for day, effort in res[1:]:
+            if effort < ans[1]:
+                ans = [day, effort]
+        return ans
+    return [-1, -1]
+
+
+mountain = [1, 2, 3, 3]
+snow = [[1,0,0,1], [0,0,0,0], [0,0,0,0], [2,1,0,0]]
+print(best_day_cross_mountain(mountain, snow))
+
