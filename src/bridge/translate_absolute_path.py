bdd0a0963b98bb3bdf208534772d978285e453ca
leowcy <leowcy@gwmail.gwu.edu>
2024-12-01 23:19:37 -0500

    ac update solution with stack

--- src/bridge/translate_absolute_path.py
+++ src/bridge/translate_absolute_path.py
@@ -1,20 +1,37 @@
+# def translate_absolute_path(input: str) -> str:
+#     input_split_by_slash = input.split("/")
+
+#     res = "/"
+#     previous_char_len = 0
+
+#     for i in range(1, len(input_split_by_slash)):
+#         if input_split_by_slash[i] == ".":
+#             continue # nothing happen for same level
+#         elif input_split_by_slash[i] == "..":
+#             end_index = len(res) - previous_char_len
+#             res = res[0:end_index]
+#         else:
+#             # for normal char
+#             previous_char_len = len(input_split_by_slash[i]) + 1
+#             res += input_split_by_slash[i] + "/"    
+#     return res[0:len(res)-1] if len(res) > 1 and res[-1] == "/" else res
+
+
 def translate_absolute_path(input: str) -> str:
-    input_split_by_slash = input.split("/")
+    s = input.split("/")
+    st = []
 
-    res = "/"
-    previous_char_len = 0
-
-    for i in range(1, len(input_split_by_slash)):
-        if input_split_by_slash[i] == ".":
-            continue # nothing happen for same level
-        elif input_split_by_slash[i] == "..":
-            end_index = len(res) - previous_char_len
-            res = res[0:end_index]
+    for i in range(1, len(s)):
+        if s[i] == "..":
+            if st:
+                st.pop()
+        elif s[i] == ".":
+            continue
         else:
-            # for normal char
-            previous_char_len = len(input_split_by_slash[i]) + 1
-            res += input_split_by_slash[i] + "/"    
-    return res[0:len(res)-1] if len(res) > 1 and res[-1] == "/" else res
+            st.append(s[i])
+    
+    return "/" + "/".join(st)
+            
 
 input_list = [
     "/ab/cd/./ef",
