e1e0b49b0f65572b2bc792b39c37e0b397d331f5
leowcy <leowcy@gwmail.gwu.edu>
2024-12-30 22:30:46 -0500

    ac

--- src/LeetCode/bfs/102-tree-level-traverse.py
+++ src/LeetCode/bfs/102-tree-level-traverse.py
@@ -24,3 +24,25 @@ class Solution:
             ans.append(values)
 
         return ans
+
+
+# Solution 2:
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+
+        ans = []
+        q = deque([root])
+        while q:
+            values = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                values.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            ans.append(values)
+
+        return ans
---
+++ src/LeetCode/bfs/103-tree-level-t-zigzag.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+
+        q = deque([root])
+        even = False
+        ans = []
+
+        while q:
+            values = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                values.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            ans.append(values) if not even else ans.append(values[::-1])
+            even = not even
+
+        return ans
