276fb53b68b6f025a1e8cb83b446a85813a60f3e
leowcy <leowcy@gwmail.gwu.edu>
2025-01-01 15:58:32 -0500

    ac 1765 again

--- src/LeetCode/bfs/1765-map-of-highest-peak.py
+++ src/LeetCode/bfs/1765-map-of-highest-peak.py
@@ -21,3 +21,29 @@ class Solution:
                     q.append((ni, nj))
 
         return ans
+
+
+# Jan 1 2025
+class Solution:
+    def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
+        m = len(isWater)
+        n = len(isWater[0])
+        ans = [[-1] * n for _ in range(m)]
+        d = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        q = deque()
+
+        for i in range(m):
+            for j in range(n):
+                if isWater[i][j] == 1:
+                    ans[i][j] = 0
+                    q.append((i, j))
+
+        while q:
+            ci, cj = q.popleft()
+            for di, dj in d:
+                ni, nj = ci + di, cj + dj
+                if 0 <= ni < m and 0 <= nj < n and ans[ni][nj] == -1:
+                    ans[ni][nj] = ans[ci][cj] + 1
+                    q.append((ni, nj))
+
+        return ans
