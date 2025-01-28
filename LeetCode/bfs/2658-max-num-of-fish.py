0f55782714f219daae11297f69a10f97157cccaf
leowcy <leowcy@gwmail.gwu.edu>
2025-01-28 00:25:23 -0500

    add dfs solution

--- LeetCode/bfs/2658-max-num-of-fish.py
+++ LeetCode/bfs/2658-max-num-of-fish.py
@@ -1,3 +1,4 @@
+# BFS
 class Solution:
     def findMaxFish(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
@@ -26,5 +27,35 @@ class Solution:
                             ):
                                 visited.add((ni, nj))
                                 water.append((ni, nj))
+
+        return ans
+
+
+# DFS
+class Solution:
+    def findMaxFish(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        d = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        ans = 0
+
+        def dfs(i, j):
+            fish = 0
+            if grid[i][j] == 0:
+                return fish
+
+            fish += grid[i][j]
+            grid[i][j] = -1
+
+            for di, dj in d:
+                ni, nj = i + di, j + dj
+                if 0 <= ni < m and 0 <= nj < n and grid[ni][nj] > 0:
+                    fish += dfs(ni, nj)
+
+            return fish
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] != 0:
+                    ans = max(ans, dfs(i, j))
 
         return ans
