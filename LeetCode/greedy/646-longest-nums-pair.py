29c2b744932ce5f36fa67051978776cbe3161127
leowcy <leowcy@gwmail.gwu.edu>
2025-02-06 22:48:59 -0500

    a

--- LeetCode/greedy/646-longest-nums-pair.py
+++ LeetCode/greedy/646-longest-nums-pair.py
@@ -8,3 +8,21 @@ class Solution:
                 temp = j
                 ans += 1
         return ans
+
+
+# 2nd time
+class Solution:
+    def findLongestChain(self, pairs: List[List[int]]) -> int:
+        pairs.sort(key=lambda x: x[1])
+        ans = 1
+        cur = pairs[0][1]
+        temp = 1
+
+        for i in range(1, len(pairs)):
+            if cur >= pairs[i][0]:
+                continue
+            temp += 1
+            cur = pairs[i][1]
+            ans = max(ans, temp)
+
+        return ans
