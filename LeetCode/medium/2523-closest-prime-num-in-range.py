f9fb23cae7e15ec76544250e77c75f0dba178db8
leowcy <leowcy@gwmail.gwu.edu>
2025-03-07 11:25:03 -0500

    add comments

--- LeetCode/medium/2523-closest-prime-num-in-range.py
+++ LeetCode/medium/2523-closest-prime-num-in-range.py
@@ -1,3 +1,7 @@
+# Precompute the prime nums list from range(0, 10**6 + 1)
+# Then we can check if num is prime in o(1) time.
+# Then we maintain a temp value for the gap and find a smaller pair we update the temp value and ans.
+# One quick check we can do is if we find a gap == 2, means the smallest gap already. We just return ans at that early step.
 mx = 10**6 + 1
 is_prime_list = [1] * mx
 for i in range(2, mx):
